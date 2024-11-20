f301767fb92b2d8ca4e12953ef4d9d98748e72f4
kun-sik <58356215+kun-sik@users.noreply.github.com>
2024-11-20 20:41:09 +0900

    fix : MainSection paragraph

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -5,7 +5,7 @@ function MainSection() {
   return (
     <section id="main" className="main-section">
       <h1>Welcome to MEEUM</h1>
-      <p>자연에서 얻은 최상의 영양을 반려동물에게 제공합니다.</p>
+      <p>자연에서 얻은 최상의 영양을 반려동물에게 야야야야</p>
     </section>
   );
 }
